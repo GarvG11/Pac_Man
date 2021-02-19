f26139ab5e6b31ce5af229acef102516918142e8
GarvG11 <73347193+GarvG11@users.noreply.github.com>
2021-02-19 17:12:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+// assigning all the game variables
+var pacman, enemy1, enemy2, enemy3, enemy4, infectedCoins
+// assigning all the image variables
+var pacmanAnimeRight,pacmanAnimeLeft,pacmanAnimeDown,pacmanAnimeUp, enemy1Img, enemy2Img, enemy3Img, enemy4Img
+
+// adding the coins
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+var coin1 = createSprite();  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function preload() {
+   
+}
+
+function setup(){
+   createCanvas(500,500);
+   pacman = createSprite()
+}
+
+function draw(){
+background('black');
+drawSprites();
+}
